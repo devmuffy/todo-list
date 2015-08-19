87a2dd776c8f1089a00509a21cb19d758eba6094
Sebastian <muffy19@gmail.com>
2015-08-19 13:46:19 +0200

    ItemClient: unit tests

--- js/utils/__tests__/ItemClient-test.js
+++ js/utils/__tests__/ItemClient-test.js
@@ -1,9 +1,55 @@
-var ItemClient = require('../ItemClient');
+jest.dontMock('../ItemClient');
+
+/**
+ * mock localStorage
+ */
+window.localStorage = (function () {
+  var store = {};
+
+  return {
+    clear: function () {
+      store = {};
+    },
+
+    getItem: function (key) {
+      return store[key];
+    },
+
+    setItem: function (key, value) {
+      store[key] = value;
+    }
+  };
+}());
+
+beforeEach(function () {
+  var store = {};
+
+  spyOn(localStorage, 'getItem').andCallFake(function (key) {
+    return store[key];
+  });
+  spyOn(localStorage, 'setItem').andCallFake(function (key, value) {
+    return store[key] = value + '';
+  });
+  spyOn(localStorage, 'clear').andCallFake(function () {
+      store = {};
+  });
+});
 
 describe('ItemClient', function () {
 
-  it('should return callbacks indexes', function () {
-    expect(false).toBe(1);
+  var ItemClient = require('../ItemClient');
+
+  it('saves the data and loads it', function () {
+    var data = {
+      someData: 'example data'
+    };
+
+    ItemClient.save(data);
+    expect(ItemClient.load()).toEqual(data);
+  });
+
+  it('loads the data', function () {
+    expect(ItemClient.load()).toEqual({});
   });
 
 });
